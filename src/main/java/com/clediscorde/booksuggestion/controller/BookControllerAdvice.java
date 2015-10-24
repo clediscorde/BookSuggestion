51fc892729f12af4f8231f3269d7011ea549bf8f
Carine Le Discorde <carine.lediscorde@gmail.com>
2015-10-24 00:24:01 -0400

    Return the exception message as response

--- src/main/java/com/clediscorde/booksuggestion/controller/BookControllerAdvice.java
+++ src/main/java/com/clediscorde/booksuggestion/controller/BookControllerAdvice.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by clediscorde on 2015-10-24.
@@ -14,6 +15,7 @@ public class BookControllerAdvice {
     private final static Logger LOG = LoggerFactory.getLogger(BookControllerAdvice.class);
 
     @ExceptionHandler(Exception.class)
+    @ResponseBody
     public String exception(Exception e) {
         LOG.error("Exception: ", e);
         return e.getMessage();
